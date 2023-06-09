1b2029797312e6b8a3ad3066d1b1cd6d298ab480
ldoron111 <ldoron111@gmail.com>
2023-06-09 18:13:29 +0300

    fixed eval code

--- helper_code/eval_model.py
+++ helper_code/eval_model.py
@@ -7,13 +7,15 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
+from helper_code.train import calculate_accuracy
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(17)
 
 def hist_correct_world_level(model):
     print("calculating world level hist")
 
-    mario_dataset_test = MarioButtonsDataset(img_dir='./mario_dataset',group_frames=3,use_color=False,worlds=TEST_WORLDS,preload=preload)
+    mario_dataset_test = MarioButtonsDataset(img_dir='./mario_dataset',group_frames=3,use_color=False,worlds=TEST_WORLDS,preload=False)
     data_loader = torch.utils.data.DataLoader(mario_dataset_test,batch_size=128,shuffle=True,num_workers=8)
     model.eval() # put in evaluation mode,  turn of DropOut, BatchNorm uses learned statistics
     
@@ -28,9 +30,17 @@ def hist_correct_world_level(model):
             correct_labels = (predicted == buttons).cpu().numpy()
             world_level = world_level.cpu().numpy()
             for i in range(correct_labels.shape[0]):
+                if world_level[i] not in world_level_hist:
+                    world_level_hist[world_level[i]] = 0
                 world_level_hist[world_level[i]] += correct_labels[i].sum()
             #print(f"size of predicted: {buttons.size()}")
     print(f"world-level correct hist :{world_level_hist}")
     return world_level_hist
+
+def calc_accuracy(model):
+    mario_dataset_test = MarioButtonsDataset(img_dir='./mario_dataset',group_frames=3,use_color=False,worlds=TEST_WORLDS,preload=False)
+    data_loader = torch.utils.data.DataLoader(mario_dataset_test,batch_size=128,shuffle=True,num_workers=8)
+    model.eval() # put in evaluation mode,  turn of DropOut, BatchNorm uses learned statistics
+    calculate_accuracy(model,data_loader,device)
 
 
