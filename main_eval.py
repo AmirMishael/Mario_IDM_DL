f5e7f243b5d7031b95ad0b8f1f9faf399fe46710
ldoron111 <ldoron111@gmail.com>
2023-06-17 14:04:43 +0300

    fixed groups eval

--- main_eval.py
+++ main_eval.py
@@ -15,10 +15,12 @@ modles_path = pathlib.Path("./models")
 
 group_size = 15
 use_color = False
-models_to_check = ["checkpoints/checkpoint_1_3500.pt","checkpoints/checkpoint_2_3500.pt","checkpoints/checkpoint_3_3500.pt"
-                   ,"checkpoints/checkpoint_4_3500.pt","checkpoints/checkpoint_5_3500.pt","checkpoints/checkpoint_6_3500.pt"]
+models_to_check = ["checkpoints/checkpoint_1_3500_group_15_color_False.pt","checkpoints/checkpoint_2_3500_group_15_color_False.pt",
+                   "checkpoints/checkpoint_3_3500_group_15_color_False.pt"
+                   ,"checkpoints/checkpoint_4_3500_group_15_color_False.pt","checkpoints/checkpoint_5_3500_group_15_color_False.pt"
+                   ,"checkpoints/checkpoint_6_3500_group_15_color_False.pt"]
 for model_name in models_to_check:
     print(f"checking model: {model_name}")
     model = ResnetModel(group_size=group_size,use_color=use_color,use_pretrained=False).to(device)
-    model.load_state_dict(torch.load(os.path.join(modles_path,model_name))
+    model.load_state_dict(torch.load(os.path.join(modles_path,model_name)))
     calc_accuracy(model,mode='val',group_frames=group_size,use_color=False)
