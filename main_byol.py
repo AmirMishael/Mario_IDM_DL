dee729de2b7f1928789cc262afbcab03a6dd5bea
ldoron111 <ldoron111@gmail.com>
2023-06-18 20:01:42 +0300

    fixed byol to use more channels

--- main_byol.py
+++ main_byol.py
@@ -1,9 +1,8 @@
 import os
 import pathlib
 import torch
-from byol_pytorch import BYOL
-from torchvision import models
-
+import kornia.augmentation as K
+from helper_code.my_byol import BYOL
 from helper_code.resnet_model import ResnetModel
 from helper_code.mario_buttons_dataset import TRAIN_WORLDS, VAL_WORLDS, MarioButtonsDataset
 
@@ -18,7 +17,7 @@ group = 15
 use_color = False
 batch_size = 128
 
-model = ResnetModel(group_size=group_size,use_color=use_color,use_pretrained=False).to(device)
+model = ResnetModel(group_size=group,use_color=use_color,use_pretrained=False).to(device)
 model.load_state_dict(torch.load(os.path.join(modles_path,model_name)))
 
 mario_dataset_train = MarioButtonsDataset(img_dir='./mario_dataset',group_frames=group,use_color=use_color,worlds=TRAIN_WORLDS,preload=False)
@@ -33,8 +32,14 @@ val_loader = torch.utils.data.DataLoader(mario_dataset_val,batch_size=batch_size
 
 learner = BYOL(
     model,
+    channels=group*3 if use_color else group,
     image_size = 256,
-    hidden_layer = 'avgpool'
+    hidden_layer = 'avgpool',
+    augment_fn=K.AugmentationSequential(K.RandomGaussianBlur(p=0.5),
+                                        K.RandromRotation(degrees=5,p=0.5)),
+    augment_fn2=K.AugmentationSequential(K.RandomResizedCrop(p=0.5,size=(256,256)),
+                                         K.RandomInvert(p=0.5)),
+
 )
 
 
@@ -44,7 +49,7 @@ def sample_unlabelled_images():
     images, _ = next(iter(train_loader))
     return images
 
-for _ in range(100):
+for _ in range(10000):
     images = sample_unlabelled_images()
     loss = learner(images)
     opt.zero_grad()
