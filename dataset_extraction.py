b4fa9656d5e7702bc4d7abaef9c4a8703bcdb584
ldoron111 <ldoron111@gmail.com>
2023-06-23 17:01:55 +0300

    added grayscale

--- dataset_extraction.py
+++ dataset_extraction.py
@@ -13,6 +13,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(17)
 transform = torchvision.transforms.Compose([
             torchvision.transforms.Resize((256,256)),
+            torchvision.transforms.Grayscale(num_output_channels=1),
             torchvision.transforms.ToTensor(),
 
             #torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
